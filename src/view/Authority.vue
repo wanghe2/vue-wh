0cab9804c4e17f1ffd49d6c2503e9a1f05b771d2
wanghe <2290370311@qq.com>
2020-04-01 17:30:59 +0800

    带有导航栏的基础页面

---
+++ src/view/Authority.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="authority">
+        <el-container>
+            <el-header>
+                虞美人
+            </el-header>
+            <el-main>
+                <el-row >
+                <el-button>默认按钮</el-button>
+                <el-button type="primary">主要按钮</el-button>
+                <el-button type="success">成功按钮</el-button>
+                <el-button type="info">信息按钮</el-button>
+                <el-button type="warning">警告按钮</el-button>
+                <el-button type="danger">危险按钮</el-button>
+                </el-row>
+
+                <el-row row-style="height:20px">
+                <el-button plain>朴素按钮</el-button>
+                <el-button type="primary" plain>主要按钮</el-button>
+                <el-button type="success" plain>成功按钮</el-button>
+                <el-button type="info" plain>信息按钮</el-button>
+                <el-button type="warning" plain>警告按钮</el-button>
+                <el-button type="danger" plain>危险按钮</el-button>
+                </el-row>
+
+                <el-row>
+                <el-button round>圆角按钮</el-button>
+                <el-button type="primary" round>主要按钮</el-button>
+                <el-button type="success" round>成功按钮</el-button>
+                <el-button type="info" round>信息按钮</el-button>
+                <el-button type="warning" round>警告按钮</el-button>
+                <el-button type="danger" round>危险按钮</el-button>
+                </el-row>
+
+                <el-row>
+                <el-button icon="el-icon-search" circle></el-button>
+                <el-button type="primary" icon="el-icon-edit" circle></el-button>
+                <el-button type="success" icon="el-icon-check" circle></el-button>
+                <el-button type="info" icon="el-icon-message" circle></el-button>
+                <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+                <el-button type="danger" icon="el-icon-delete" circle></el-button>
+                </el-row>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+<style scoped>
+#authority{
+    margin-top: -20px;
+}
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 100px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+</style>
---
+++ src/view/Role.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="role">
+        <el-container>
+        <el-header>木兰词</el-header>
+        <el-main>人生若只如初见，何事秋风悲画扇</el-main>
+        <el-footer>清明记</el-footer>
+        </el-container>
+    </div>
+</template>
+<script>
+export default {
+    name:'role'
+}
+</script>
+<style scoped>
+  #role {
+       margin-top: -20px;
+  }
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 300px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
